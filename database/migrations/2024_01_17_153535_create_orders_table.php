85d5c2ea4efcda9908ffa198adb15683dc9a2832
MohammadMalek <mimdeveloper.tv@gmail.com>
2024-01-17 18:03:50 +0330

    done API Strcut And User Auth(s) Operation

---
+++ database/migrations/2024_01_17_153535_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('price', 15);
+            $table->boolean('notifyAdmin')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
